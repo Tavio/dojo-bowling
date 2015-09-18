407ba76800a5c3b31400a1d33e4a0e1f99a9e215
Otavio Nascimento <otanasc@gmail.com>
2015-09-18 19:41:30 +1000

    adding a couple of test cases

--- spec/bowling/game_calculator_spec.rb
+++ spec/bowling/game_calculator_spec.rb
@@ -20,5 +20,13 @@ describe GameCalculator do
     it 'calculates the score for a game with spares on every round correctly' do
       subject.calculate('5/5/5/5/5/5/5/5/5/5/5').should == 150
     end
+    
+    it 'calculates the correct score for this game that I found on the internet' do
+      subject.calculate("X9/5/72XXX9-8/9/X").should == 187
+    end
+
+    it 'calculates the correct score for this other game that I found on the internet' do
+      subject.calculate("63718/72X627/X8-7/X").should == 135
+    end
   end
 end
